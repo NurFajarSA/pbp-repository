9b8e5048de332c1af866252c161fef80b29d29d4
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-09-14 14:46:45 +0700

    develop mvt

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from katalog.views import show_catalog
 # TODO: Implement Routings Here
+
+app_name = 'katalog'
+urlpatterns = [
+    path('', show_catalog, name='catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    catalog_data = CatalogItem.objects.all()
+    context = {
+        'catalog_data': catalog_data,
+        'name': 'Nur Fajar',
+        'id': '2106751114'
+    }
+    return render(request, 'katalog.html', context)
