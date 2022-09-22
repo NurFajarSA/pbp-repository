565efa03afa77f630c87352861f224057d2de97e
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-09-22 11:05:20 +0700

    bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,8 +6,18 @@ from django.core import serializers
 # Create your views here.
 def show_html(request):
     data = MyWatchlist.objects.all()
+    total_data = MyWatchlist.objects.count()
+    cnt_watched = 0
+    for i in data:
+        if i.watched:
+            cnt_watched += 1
+            
+    message = "Selamat, kamu sudah banyak menonton!"
+    if cnt_watched < total_data:
+        message = "Wah, kamu masih sedikit menonton!"
     context = {
         'data': data,
+        'message': message
     }
     return render(request, 'mywatchlist.html', context)
 
