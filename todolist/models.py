ec1caa7a56daeb091f690a7ad7a7edaad9887808
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-09-27 08:47:19 +0700

    create task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,14 @@
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    date = models.DateField(default=date.today)
+    description = models.TextField(blank=True, null=True)
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
